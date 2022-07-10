333834fac30013926e40e15aad846ac1229b7b25
undeceive <GitHubClient@protonmail.com>
2022-07-10 14:07:17 -0400

    Main Component

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Card, CardHeader, CardContent, Typography, Grid, Divider } from '@material-ui/core';
+
+import useStyles from './styles';
+
+const Main = () => {
+  const classes = useStyles();
+
+  return (
+    <Card className={classes.root}>
+      <CardHeader title="Expense Tracker" subheader="Powered by Speechly"/>
+      <CardContent>
+        <Typography align="center" variant="h5">Total Balance $100</Typography>
+        <Typography variant="subtitle1" style={{ lineHeight: '1.5em', margintop: '20px' }}>
+        {/* infoCard... */}
+        Try saying: Add income for $100 in Category Salary for Monay...
+          </Typography>
+        <Divider />
+        {/* Form */}
+      </CardContent>
+    <CardContent className={classes.cardContent}>
+        <Grid contianer spacing={2}>
+          <Grid item xs={12}>
+            {/* <List/> */}
+          </Grid>
+        </Grid>
+      </CardContent>
+    </Card>
+  )
+}
+
+export default Main
