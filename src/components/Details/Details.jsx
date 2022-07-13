1b1118c262f24532039893bdc2aba665f10473ec
undeceive <GitHubClient@protonmail.com>
2022-07-13 12:37:19 -0400

    Adding charts

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,21 +1,27 @@
 import React from 'react';
 import { Card, CardHeader, CardContent, Typography } from '@material-ui/core';
 import { Doughnut } from 'react-chartjs-2';
+import useTransactions from '../../useTransactions';
+import {Chart, ArcElement} from 'chart.js'
 
 import useStyles from './styles';
 
+
+Chart.register(ArcElement);
+
 const Details = ({ title }) => {
   const classes = useStyles();
+  const { total, chartData } = useTransactions(title);
 
   return (
-    <Card className={ title === 'Income' ? classes.income : classes.expense}>
-      <CardHeader title={title}/>
+    <Card className={title === 'Income' ? classes.income : classes.expense}>
+      <CardHeader title={title} />
       <CardContent>
-        <Typography variant="h5">$50</Typography>
-        {/*<Doughnut data="DATA"/>*/}
+        <Typography variant="h5">${total}</Typography>
+          <Doughnut data={chartData}/>
       </CardContent>
     </Card>
-  )
+  );
 }
 
-export default Details;
+export default Details
